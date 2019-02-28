98fd874b08aa8a874234b6d219afd0989130ca06
Tri Budiyono <tribudiyono93@gmail.com>
2019-02-28 19:09:45 +0700

    first init

---
+++ calls_function.php
@@ -0,0 +1,10 @@
+<?php 
+  
+echo testFunc(5, 6); 
+  
+function testFunc($num1, $num2) { 
+   $val = $num1 + $num2; 
+   return $val; 
+} 
+  
+?> 
---
+++ coding_standart.php
@@ -0,0 +1,14 @@
+<?php 
+  
+$n = 5; 
+if ($n > 0){ 
+    echo "Positive"; 
+} 
+elseif ($n < 0){ 
+    echo "Negative"; 
+} 
+else{ 
+    echo "Zero"; 
+} 
+  
+?> 
